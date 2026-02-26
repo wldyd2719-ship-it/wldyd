2763da2124a795e2356815d86bc634d5a1f442f1
wldyd2719-ship-it <wldyd2719@gmail.com>
2026-02-26 11:39:36 +0900

    Add files via upload

---
+++ port/codes/0205/picM.py
@@ -0,0 +1,9 @@
+import urllib.request
+import zipfile
+url = "https://storage.googleapis.com/learning-datasets/horse-or-human.zip"
+file_name = "horse-or-human.zip"
+training_dir = 'horse-or-human/training/'
+urllib.request.urlretrieve(url, file_name)
+zip_ref = zipfile.ZipFile(file_name, 'r')
+zip_ref.extractall(training_dir)
+zip_ref.close()
